03b427fc41250bf2a11138462ed0e02608adf781
Rani Deshmukh <ranideshmukh034@gmail.com>
2020-01-25 12:48:01 +0530

    Commiting first java class

---
+++ BasicCoreJava/src/VariablesAndDataTypes/Firstjavaclass.java
@@ -0,0 +1,11 @@
+package VariablesAndDataTypes;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to the World of Automation");
+        
+	}
+
+}
