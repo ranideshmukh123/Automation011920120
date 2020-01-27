fbdf8a037746873d5c39a07a3a1800b51e2b90c3
Rani Deshmukh <ranideshmukh034@gmail.com>
2020-01-27 11:58:13 +0530

    Commiting while loop

---
+++ BasicCoreJava/src/Loops/Whileloop.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		
+		int i=0;  //Initialization
+		
+		while(i<=10)  //Condition
+			
+		{
+			System.out.println(i);
+			i++; //Increment or Decrement
+		}
+		
+
+	}
+
+}
