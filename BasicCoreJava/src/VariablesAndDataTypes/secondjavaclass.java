d354732810f0589823c43279e32f386167da9bdf
Rani Deshmukh <ranideshmukh034@gmail.com>
2020-01-27 12:00:50 +0530

    commiting second java class

---
+++ BasicCoreJava/src/VariablesAndDataTypes/secondjavaclass.java
@@ -0,0 +1,30 @@
+package VariablesAndDataTypes;
+
+public class secondjavaclass {
+	
+public static void main(String[] args) {
+	
+	int age=27;
+	String name="Rani";
+	boolean status=true;
+	char gender='f';
+	double grade=6.5; 
+	
+	
+	System.out.println(age);
+	
+	//Integer
+	byte a=10;
+	short b=20;
+	int c=30;
+	long d=40;
+	
+	
+	//Decimal
+	float e=59.56f;
+    double f=45.768;
+    
+	
+
+}
+}
